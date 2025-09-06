3b816a03d399b1315c41fc30c6b75eaf95453ffb
Aman <aman.swar001@gmail.com>
2025-09-07 03:45:07 +0530

    mv config -> config.py

--- llm/qwen3/qwen_torch.py
+++ llm/qwen3/qwen_torch.py
@@ -1,22 +1,8 @@
 import torch
 import torch.nn as nn
 
-from dataclasses import dataclass
+from qwen3.config import QwenConfig
 
-@dataclass
-class QwenConfig:
-    vocab_size : int = 151936
-    context_length : int = 32000
-    embed_dim : int = 1024
-    n_heads : int = 16
-    n_layers : int = 28
-    head_dim : int = 128
-    n_kv_grps : int = 2 # since qwen3 0.6B have 16/8 split of Q and KV heads -> 16 / 8 -> grps of 2
-    hidden_dim : int = 3072
-    qk_norm : bool = True
-    rope_base : float = 1e6
-    dtype : torch.dtype = torch.bfloat16
-    
 
 def compute_rope_params(
     head_dim,
