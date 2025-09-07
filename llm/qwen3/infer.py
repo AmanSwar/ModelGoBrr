f12b154f337c23ea51645d50daa329914b1264f9
Aman <aman.swar001@gmail.com>
2025-09-07 17:28:42 +0530

    feat : model loading

--- llm/qwen3/infer.py
+++ llm/qwen3/infer.py
@@ -6,10 +6,17 @@ from qwen3.config import QwenConfig
 from utils import model_mem_size
 from qwen3.load import load_weights
 
+
+from pathlib import Path
+import os
+from huggingface_hub import snapshot_download
+from safetensors.torch import load_file
+
 torch.manual_seed(696969)
 device = torch.device("cuda")
-config = QwenConfig()
 
+
+config = QwenConfig()
 model = Qwen3(config).to(device)
 
 #checking the model 
@@ -18,3 +25,9 @@ model = Qwen3(config).to(device)
 print(f"float32 (PyTorch default): {model_mem_size(model, input_dtype=torch.float32):.2f} GB")
 print(f"bfloat16: {model_mem_size(model, input_dtype=torch.bfloat16):.2f} GB")
 
+repo_dir = "/home/aman/code/model_go_brr/llm/Qwen3-0.6B"
+single_file_path = os.path.join(repo_dir, "model.safetensors")
+weights_dict = load_file(single_file_path)
+load_weights(model, config, weights_dict)
+model.to(device)
+print("Model loaded successfully!")
