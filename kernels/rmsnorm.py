34b48925914286b49a6fbcc2e66d96eec2c2d807
Aman <aman.swar001@gmail.com>
2025-09-10 10:54:32 +0530

    major fix : made block_size constant to avoid recompilation

--- kernels/rmsnorm.py
+++ kernels/rmsnorm.py
@@ -7,6 +7,58 @@ from torch.autograd import Function
 
 from .config_tool import tuner
 
+# @triton.jit
+# def _rmsnorm_fwd(
+#     input_matrix,
+#     output_matrix,
+#     weight_matrix,
+#     M , N,
+#     eps,
+#     BLOCK_SIZE : tl.constexpr
+
+# ):
+
+#     row_index = tl.program_id(0)
+
+#     row_start = row_index * N
+
+#     cols_offset = tl.arange(0 , BLOCK_SIZE)
+
+#     input_ptrs = input_matrix + row_start + cols_offset
+
+#     mask = cols_offset < N
+
+#     row = tl.load(input_ptrs , mask=mask , other=0.0)
+#     weight = tl.load(weight_matrix + cols_offset , mask=mask , other=1.0)
+
+#     _rms = tl.rsqrt(((tl.sum(row * row))/N) + eps)
+
+#     out_row = (row * _rms) * weight
+
+#     output_ptrs = output_matrix + row_start + cols_offset
+#     tl.store(output_ptrs , out_row , mask=mask)
+
+# @triton.autotune(
+#     [
+#         triton.Config(
+#             {"BLOCK_SIZE": BLOCK_SIZE},
+#             num_stages=num_stages,
+#             num_warps=num_warps,
+#         )
+#         for BLOCK_SIZE in [32 ,64, 128 , 256]
+#         for num_stages in ([3, 4, 7])
+#         for num_warps in [2, 4]
+#     ],
+#     key=["N"],
+# )
+
+"""
+Best config :
+BLOCK_SIZE : 256
+num_warps: 2
+num_ctas: 1
+num_stages: 4
+"""
 @triton.jit
 def _rmsnorm_fwd(
     input_matrix,
@@ -15,80 +67,85 @@ def _rmsnorm_fwd(
     M , N,
     eps,
     BLOCK_SIZE : tl.constexpr
-    
 ):
-    
     row_index = tl.program_id(0)
-
     row_start = row_index * N
-    
-    cols_offset = tl.arange(0 , BLOCK_SIZE)
+    _sum_sq = 0.0
+    for block in range(tl.cdiv(N , BLOCK_SIZE)):
+        col_ptrs = block * BLOCK_SIZE + tl.arange(0 , BLOCK_SIZE)
+        input_ptrs = input_matrix + row_start + col_ptrs
+        mask = col_ptrs < N
+        row_block = tl.load(input_ptrs , mask=mask , other=0.0)
+        _sum_sq += tl.sum(row_block * row_block)
 
-    input_ptrs = input_matrix + row_start + cols_offset
+    _rms = tl.rsqrt((_sum_sq/ N) + eps)
 
-    mask = cols_offset < N
-    
-    row = tl.load(input_ptrs , mask=mask , other=0.0)
-    weight = tl.load(weight_matrix + cols_offset , mask=mask , other=1.0)
+    for block in range(tl.cdiv(N , BLOCK_SIZE)):
+        col_ptrs = block * BLOCK_SIZE + tl.arange(0 , BLOCK_SIZE)
+        input_ptrs = input_matrix + row_start + col_ptrs
+        output_ptrs = output_matrix + row_start + col_ptrs
+        weight_ptrs = weight_matrix + col_ptrs
 
-    _rms = tl.rsqrt(((tl.sum(row * row))/N) + eps)
+        mask = col_ptrs < N
+        row_block = tl.load(input_ptrs, mask=mask , other=0.0)
+        weight_block = tl.load(weight_ptrs , mask=mask , other=1.0)
 
-    out_row = (row * _rms) * weight
+        out_row = (row_block * _rms) * weight_block
 
-    output_ptrs = output_matrix + row_start + cols_offset
-    tl.store(output_ptrs , out_row , mask=mask)
-
-@triton.jit
-def _rmsnorm_bwd(
-    input_matrix,
-    weight_matrix,
-    grad_matrix,
-    grad_x_matrix,
-    grad_w_acc_matrix,
-    M , N ,
-    eps,
-    BLOCKS_SIZE : tl.constexpr,
-):
-
-    #base indexing
-    row_index = tl.program_id(0)
-    cols_offset = tl.arange(0,BLOCKS_SIZE)
-
-    #ptrs for all 
-    input_ptrs = input_matrix + row_index * N + cols_offset
-    grad_ptrs = grad_matrix + row_index * N + cols_offset
-    weight_ptrs = weight_matrix + cols_offset
-
-    grad_w_ptrs = grad_w_acc_matrix + row_index * N + cols_offset
-    grad_x_ptrs = grad_x_matrix + row_index * N + cols_offset
-
-    mask = cols_offset < N
-
-    #load
-    input_row = tl.load(input_ptrs , mask=mask , other=0.0)
-    grad_row = tl.load(grad_ptrs , mask=mask , other=0.0)
-    weight = tl.load(weight_ptrs , mask=mask  ,other=0.0)
+        tl.store(output_ptrs , out_row , mask=mask)
 
 
-    #recompute RMS
-    _rms = tl.sqrt((tl.sum(input_row * input_row) / N) +eps)
-    
+# @triton.jit
+# def _rmsnorm_bwd(
+#     input_matrix,
+#     weight_matrix,
+#     grad_matrix,
+#     grad_x_matrix,
+#     grad_w_acc_matrix,
+#     M , N ,
+#     eps,
+#     BLOCKS_SIZE : tl.constexpr,
+# ):
 
-    grad_weight_partial = (input_row / _rms) * grad_row
+#     #base indexing
+#     row_index = tl.program_id(0)
+#     cols_offset = tl.arange(0,BLOCKS_SIZE)
 
-    tl.store(grad_w_ptrs,grad_weight_partial,mask=mask)
+#     #ptrs for all
+#     input_ptrs = input_matrix + row_index * N + cols_offset
+#     grad_ptrs = grad_matrix + row_index * N + cols_offset
+#     weight_ptrs = weight_matrix + cols_offset
 
-    _first_term = (grad_row/ _rms) * weight
+#     grad_w_ptrs = grad_w_acc_matrix + row_index * N + cols_offset
+#     grad_x_ptrs = grad_x_matrix + row_index * N + cols_offset
 
-    _second_term_num = tl.sum(input_row * weight * grad_row)
-    _second_term_denom = N + _rms * _rms * _rms
+#     mask = cols_offset < N
 
-    grad_x = _first_term - (_second_term_num / _second_term_denom)
+#     #load
+#     input_row = tl.load(input_ptrs , mask=mask , other=0.0)
+#     grad_row = tl.load(grad_ptrs , mask=mask , other=0.0)
+#     weight = tl.load(weight_ptrs , mask=mask  ,other=0.0)
 
-    tl.store(grad_x_ptrs , grad_x , mask=mask)
+
+#     #recompute RMS
+#     _rms = tl.sqrt((tl.sum(input_row * input_row) / N) +eps)
 
 
-class RMSNormTritonFunction(Function):
+#     grad_weight_partial = (input_row / _rms) * grad_row
+
+#     tl.store(grad_w_ptrs,grad_weight_partial,mask=mask)
+
+#     _first_term = (grad_row/ _rms) * weight
+
+#     _second_term_num = tl.sum(input_row * weight * grad_row)
+#     _second_term_denom = N + _rms * _rms * _rms
+
+#     grad_x = _first_term - (_second_term_num / _second_term_denom)
+
+#     tl.store(grad_x_ptrs , grad_x , mask=mask)
+
+
+class _RMSNormTritonFunction(Function):
 
     @staticmethod
     def forward(ctx , x : torch.Tensor , weight , eps=1e-6):
@@ -104,9 +161,11 @@ class RMSNormTritonFunction(Function):
         x_2d_view = x.reshape(M , N)
         y = torch.empty_like(x_2d_view)
 
-        BLOCK_SIZE , num_warps = tuner(N)
         grid = (M,)
-
+        BLOCK_SIZE = 256
+        num_warps= 2
+        num_ctas= 1
+        num_stages= 4
         _rmsnorm_fwd[grid](
             input_matrix=x_2d_view,
             output_matrix=y,
@@ -115,7 +174,9 @@ class RMSNormTritonFunction(Function):
             N=N,
             eps=eps,
             BLOCK_SIZE=BLOCK_SIZE,
-            num_warps=num_warps
+            num_warps=num_warps,
+            num_ctas=num_ctas,
+            num_stages=num_stages
         )
 
         ctx.save_for_backward(x_2d_view , weight)
@@ -127,41 +188,11 @@ class RMSNormTritonFunction(Function):
 
     @staticmethod
     def backward(ctx, grad_outputs):
-
-        x_2d_view , weight = ctx.saved_tensors
-        eps = ctx.eps
-        N = ctx.N
-        original_shape = ctx.original_shape
-
-        grad_outputs = grad_outputs.contiguous().view(x_2d_view.shape)
-
-        M = x_2d_view.shape[0]
-
-        grad_x = torch.zeros_like(x_2d_view)
-        grad_weight = torch.zeros_like(weight)
-
-        grid = (M,)
-        BLOCK_SIZE , num_warps = tuner(N)
-
-        _rmsnorm_bwd[grid](
-            input_matrix=x_2d_view,
-            weight_matrix=weight,
-            grad_matrix=grad_outputs,
-            grad_x_matrix=grad_x,
-            grad_w_acc_matrix=grad_weight,
-            M=M,
-            N=N,
-            eps=eps,
-            BLOCKS_SIZE= BLOCK_SIZE,
-            num_warps=num_warps
-        )
-
-
-        return grad_x , grad_weight
+        raise NotImplementedError
 
 
 def rmsnorm_triton(x , weight , eps=1e-6):
-    return RMSNormTritonFunction.apply(x  ,weight , eps)
+    return _RMSNormTritonFunction.apply(x  ,weight , eps)
 
 
 class RMSNormTriton(torch.nn.Module):
