01362889d2bb489bb41b3562bce7d2a898d16d45
Aman <aman.swar001@gmail.com>
2025-09-24 02:50:33 +0530

    benchmarking function updated

--- llm/qwen3/bench.py
+++ llm/qwen3/bench.py
@@ -2,7 +2,6 @@ import torch
 
 import time
 
-device = torch.device("cuda")
 
 def generate(model, tokenizer, prompt, max_new_tokens=128):
     tokens = tokenizer.encode(prompt)
@@ -34,6 +33,8 @@ def benchmark_generation(
     for _ in range(warmup):
         _ = generate(model, tokenizer, prompt, max_new_tokens=32)
 
+    total_latency = 0
+    total_token_sec = 0
     t0 = time.monotonic()
     outputs = []
     for i in range(iters):
@@ -45,7 +46,47 @@ def benchmark_generation(
         in_len = len(tokenizer.encode(prompt))
         gen_len = len(tokenizer.encode(out)) - in_len
 
+        total_latency += latency
+        total_token_sec += (gen_len / latency)
         print(
             f"iter {i+1:2d}: latency={latency:.3f}s, tokens/s={gen_len/latency:.2f}"
         )
         outputs.append((latency, out))
+    
+    print(f"Token : {max_new_tokens}: average latency = {total_latency / iters}  | average token/s = {total_token_sec / iters}")
+
+    return (total_latency/iters , total_token_sec/iters)
+
+if __name__ == "__main__":
+
+    from llm.qwen3.qwen_torch import Qwen3
+    from llm.qwen3.fast_qwen_cuda import FastQwen3
+    from llm.qwen3.qwen_token import Qwen3Tokenizer
+    from llm.qwen3.config import QwenConfig_bfloat16 , QwenConfig_float16
+
+    device = torch.device("cuda")
+    tokenizer_file_path = "/home/aman/code/model_go_brr/Qwen3-0.6B/tokenizer.json"
+
+    config_qwen_bf16 =QwenConfig_bfloat16()
+    config_qwen_fp16 = QwenConfig_float16()
+    tokenizer = Qwen3Tokenizer(
+        tokenizer_file_path=tokenizer_file_path,
+        add_gen_prompt=True,
+        add_thinking=True,
+    )
+
+    model_torch = Qwen3(config_qwen_bf16).to(device)
+    model_fast = FastQwen3(config_qwen_fp16).to(device)
+
+
+    max_new_token_range = [32 , 64 , 128 , 256 , 512 , 1024]
+
+    latency = []
+    token_per_sec = []
+
+    print("Start benchmarking")
+    for token_size in max_new_token_range:
+        pass
+
+
+    
