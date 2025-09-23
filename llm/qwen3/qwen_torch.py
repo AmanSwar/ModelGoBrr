f292be2f573bbfec520a2528eab4d278f97954bf
Aman <aman.swar001@gmail.com>
2025-09-24 02:51:15 +0530

    add compiled model

--- llm/qwen3/qwen_torch.py
+++ llm/qwen3/qwen_torch.py
@@ -239,6 +239,7 @@ class Qwen3(nn.Module):
 if __name__ == "__main__":
 
     from llm.qwen3.qwen_token import Qwen3Tokenizer
+    from llm.qwen3.bench import benchmark_generation
     import time
     # from llm.qwen3.load import load_weights_qwen
     import os
@@ -260,54 +261,13 @@ if __name__ == "__main__":
         add_thinking=True,
     )
     model = model.to(device)
+    compiled_model = torch.compile(model)
 
     PROMPT = "Write a concise, friendly summary of why distributed training matters for large models.\n"
 
-    def generate(model, tokenizer, prompt, max_new_tokens=128):
-        tokens = tokenizer.encode(prompt)
-        tokens = (
-            torch.tensor(tokens).unsqueeze(0).to(device)
-        )  # unsequeze to include the batch dimension
-
-        start_pos = tokens.shape[1]
-        total_len = start_pos + max_new_tokens
-        st = time.monotonic()
-        for _ in range(max_new_tokens):
-            with torch.no_grad():
-                logits = model(tokens)
-                # print(f"Pass {_}")
-
-            next_token = torch.argmax(logits[:, -1, :], dim=-1).unsqueeze(0)
-            tokens = torch.cat((tokens, next_token), dim=1)
-        et = time.monotonic() - st
-        print(f"End time", et)
-        decoded = tokenizer.decode(tokens.squeeze(0).tolist())
-        return decoded
-
-    def benchmark_generation(
-        model, tokenizer, prompt=PROMPT, warmup=1, iters=5, max_new_tokens=20
-    ):
-        print("Warming up...")
-        for _ in range(warmup):
-            _ = generate(model, tokenizer, prompt, max_new_tokens=32)
-
-        t0 = time.monotonic()
-        outputs = []
-        for i in range(iters):
-            s = time.monotonic()
-            out = generate(model, tokenizer, prompt, max_new_tokens=max_new_tokens)
-            e = time.monotonic()
-            latency = e - s
-
-            in_len = len(tokenizer.encode(prompt))
-            gen_len = len(tokenizer.encode(out)) - in_len
-
-            print(
-                f"iter {i+1:2d}: latency={latency:.3f}s, tokens/s={gen_len/latency:.2f}"
-            )
-            outputs.append((latency, out))
 
     print("Start benchmarking")
-    benchmark_generation(model, tokenizer)
+    benchmark_generation(model, tokenizer , prompt=PROMPT , warmup=2 , iters=5 , max_new_tokens=128)
+    benchmark_generation(compiled_model, tokenizer , prompt=PROMPT , warmup=2 , iters=5 , max_new_tokens=128)
 
     # _ = generate(model, tokenizer, PROMPT, max_new_tokens=20)
