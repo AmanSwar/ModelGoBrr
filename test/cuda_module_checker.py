7a03a9be3121b5b64613bccc9ffd848249e070d1
Aman <aman.swar001@gmail.com>
2025-09-23 09:37:56 +0530

    module checker for custom cuda function

---
+++ test/cuda_module_checker.py
@@ -0,0 +1,6 @@
+from rmsnorm_kernel_vectorized import rmsnorm_kernel_vectorized as rmsnorm_kernel
+from rope_cuda import rope_apply_cuda
+
+print(type(rmsnorm_kernel))
+print(type(rope_apply_cuda))
+
